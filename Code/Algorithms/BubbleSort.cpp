c688d463e785ee4df1bf2289d8bbe9de8858b3c5
Z Ov3rFlow <zumariodj@gmail.com>
2016-11-16 18:26:40 +0100

    Create BubbleSort.cpp

---
+++ Code/Algorithms/BubbleSort.cpp
@@ -0,0 +1,24 @@
+/* Z Ov3rFlow */
+ 
+/*
+BubbleSort
+*/
+ 
+//Swap elements / Swap degli elementi
+static void swap(int a[], int first, int second)
+{
+    int tmp;
+    tmp = a[first];
+    a[first] = a[second];
+    a[second] = tmp;
+}
+ 
+ 
+//BubbleSort
+static void bubbleSort(int a[], int N)
+{
+    for(int i = 1; i < N; i++)
+        for(int j = 0; j < N-i; j++)
+            if(a[j] > a[j+1])
+                swap(a, j, j+1);
+}
