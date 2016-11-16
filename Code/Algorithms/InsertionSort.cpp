270c208ae164d1923b5025bdd086af3c2ddeb009
Z Ov3rFlow <zumariodj@gmail.com>
2016-11-16 18:23:05 +0100

    Create InsertionSort.cpp

---
+++ Code/Algorithms/InsertionSort.cpp
@@ -0,0 +1,23 @@
+/* Z Ov3rFlow */
+ 
+/*
+InsertionSort
+*/
+
+void insertionSort(int a[], int N)
+{
+	for(int i = 1; i < N; i++)
+	{
+		int key = a[i];
+		int position = i;
+
+		//it shifts values larger than key to right / shifta i valori più grandi di key a destra
+		while(position > 0 && a[position-1] > key)
+		{
+			a[position] = a[position-1];
+			position--;
+		}
+
+		a[position] = key;
+	}
+}
