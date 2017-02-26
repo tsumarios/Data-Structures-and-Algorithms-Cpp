f5ce4944e1835a7efe03b5fb07c1c1be350fb6a1
Mario Raciti <zumariodj@gmail.com>
2017-02-26 17:17:28 +0100

    Update BinarySearchTree Template.cpp

--- Code/Data-Structures/Trees/BinarySearchTree Template.cpp
+++ Code/Data-Structures/Trees/BinarySearchTree Template.cpp
@@ -8,207 +8,233 @@ Binary Search Tree Template
 
 using namespace std;
 
+
 /******************* Class Node *******************/
 template<class H> class Node
 {
-    Node<H> *left, *right, *parent;
-    H *key;
+		H *key;
+		Node<H> *left, *right, *parent;
 
-  public:
-    Node(H *key = NULL, Node<H> *left = NULL, Node<H> *right = NULL, Node<H> *parent = NULL)
-    {
-      if(key)   this->key = new H(*key);
-      else      this->key = NULL;
-      this->left = left;
-      this->right = right;
-      this->parent = parent;
-    }
+	public:
+		Node(H *key = NULL, Node<H> *left = NULL, Node<H> *right = NULL, Node<H> *parent = NULL)
+		{
+			if(key)	this->key = new H(*key);
+			else	this->key = NULL;
+			this->left = left;
+			this->right = right;
+			this->parent = parent;
+		}
 
-    void setLeft(Node<H> *left) { this->left = left; }
-    void setRight(Node<H> *right) { this->right = right; }
-    void setParent(Node<H> *parent) { this->parent = parent; }
-    void setKey(H *key) { this->key = new H(*key); }
+		void setLeft(Node<H> *left)	{ this->left = left; }
+		void setRight(Node<H> *right)	{ this->right = right; }
+		void setParent(Node<H> *parent)	{ this->parent = parent; }
+		void setKey(H *key)	{ this->key = new H(*key); }
 
-    Node<H>* getLeft()  { return left; }
-    Node<H>* getRight()  { return right; }
-    Node<H>* getParent()  { return parent; }
-    H* getKey()  { return new H(*key); }
-
+		Node<H>* getLeft() { return left; }
+		Node<H>* getRight() { return right; }
+		Node<H>* getParent() { return parent; }
+		H* getKey() { return new H(*key); }
 };
 
 
 /******************* Class BSTree *******************/
 template<class H> class BSTree
 {
-    Node<H> *root, *current;
-    int n;
+		int n;
+		Node<H> *root, *current;
 
-    H* _search(Node<H>* x, H e)
-    {
-			if(!x) return 0;
-			if(e == *x->getKey()) return x->getKey();
-			if(e < *x->getKey()) return _search(x->getLeft(), e);
-			else                 return _search(x->getRight(), e);
-		}
-
-    Node<H>* _searchNode(H e) {
+		Node<H>* _searchNode(H e)
+		{
 			if(!root) return NULL;
 			Node<H> *x = root;
-			while(x && *x->getKey()!= e)
-      {
+			while(x && *x->getKey() != e)
+			{
 				if(e < *x->getKey()) x = x->getLeft();
-				else                 x = x->getRight();
+				else x = x->getRight();
 			}
 			return x;
 		}
 
-    Node<H>* _minimum(Node<H> *tmp)
-    {
-      if(!tmp)  return NULL;
-      Node<H> *x = tmp;
-      while(x->getLeft()) x = x->getLeft();
-      return x;
-    }
+		Node<H>* _minimum(Node<H> *tmp)
+		{
+			if(!tmp) return NULL;
+			Node<H> *x = tmp;
+			while(x->getLeft())	x = x->getLeft();
+			return	x;
+		}
 
-    Node<H>* _succ(Node<H> *tmp)
-    {
-      if(!tmp)  return NULL;
-      if(tmp->getRight()) return _minimum(tmp->getRight());
-      Node<H> *x = tmp;
-      Node<H> *y = tmp->getParent();
-      while(y && y->getRight() == x)
-      {
-        x = y;
-        y = y->getParent();
-      }
-      return y;
-    }
+		Node<H>* _succ(Node<H> *tmp)
+		{
+			if(!tmp) return NULL;
+			if(tmp->getRight())	return _minimum(tmp->getRight());
+			Node<H> *x = tmp;
+			Node<H> *y = tmp->getParent();
+			while(y && y->getRight() == x)
+			{
+				x = y;
+				y = y->getParent();
+			}
+			return y;
+		}
 
-    BSTree<H>* _del(Node<H> *tmp)
-    {
-      if(!tmp) return this;
-			/* primo caso */
+		BSTree<H>* _del(Node<H> *tmp)
+		{
+			if(!tmp) return NULL;
+
+			/* First Case: tmp hasn't sons */
 			if(!tmp->getLeft() && !tmp->getRight())
-      {
+			{
 				Node<H> *y = tmp->getParent();
-				if(!y) root = NULL;
+				if(!y)	root = NULL;
 				else
-            if(y->getLeft() == tmp)  y->setLeft(NULL);
-					  else                     y->setRight(NULL);
-        delete tmp;
-        n--;
+					if(y->getLeft() == tmp)	y->setLeft(NULL);
+					else y->setRight(NULL);
+
+				delete(tmp);
+				n--;
 				return this;
 			}
-			/* terzo caso */
+
+			/* Third Case: tmp has both sons */
 			if(tmp->getLeft() && tmp->getRight())
-      {
-				Node<H>* s = _succ(tmp);
+			{
+				Node<H> *s = _succ(tmp);
 				tmp->setKey(s->getKey());
 				_del(s);
 				return this;
 			}
-			/* secondo caso */
+
+			/* Second Case: tmp has only one son */
 			Node<H> *y = tmp->getParent();
 			Node<H> *z = tmp->getLeft();
-			if(!z) z = tmp->getRight();
-			if(!y) root = z;
+			if(!z)	z = tmp->getRight();
+			if(!y)	root = z;
 			else
-				if(y->getLeft() == tmp) y->setLeft(z);
-				else                    y->setRight(z);
+				if(y->getLeft() == tmp)	y->setLeft(z);
+				else y->setRight(z);
 			z->setParent(y);
-      delete tmp;
-      n--;
+			delete(tmp);
+			n--;
 			return this;
-    }
+		}
 
-  public:
-    BSTree(): root(NULL), n(0) {}
+		void _preOrder(Node<H> *tmp)
+		{
+			if(!tmp) return;
+			cout << *tmp->getKey() << " ";
+			_preOrder(tmp->getLeft());
+			_preOrder(tmp->getRight());
+		}
 
-    BSTree<H>* ins(H x)
-    {
-      Node<H> *nd = new Node<H>(&x);
-      if(!root)
-      {
-        root = nd;
-        n++;
-        return this;
-      }
-      Node<H> *tmp = root;
-      Node<H> *par = NULL;
-      while(tmp)
-      {
-        par = tmp;
-        if(x < *tmp->getKey())  tmp = tmp->getLeft();
-        else                    tmp = tmp->getRight();
-      }
-      if(x < *par->getKey())  par->setLeft(nd);
-      else                    par->setRight(nd);
-      nd->setParent(par);
-      n++;
-      return this;
-    }
+		void _postOrder(Node<H> *tmp)
+		{
+			if(!tmp) return;
+			_postOrder(tmp->getLeft());
+			_postOrder(tmp->getRight());
+			cout << *tmp->getKey() << " ";
+		}
 
-    BSTree<H>* del(H x)
-    {
-      Node<H>* tmp = _searchNode(x);
+	public:
+		BSTree() : n(0), root(NULL) {}
+
+		BSTree<H>* ins(H x)
+		{
+			Node<H> *nd = new Node<H>(&x);
+			if(!root)
+			{
+				root = nd;
+				n++;
+				return this;
+			}
+
+			Node<H> *tmp = root;
+			Node<H> *par = NULL;
+			while(tmp)
+			{
+				par = tmp;
+				if(x < *tmp->getKey())	tmp = tmp->getLeft();
+				else	tmp = tmp->getRight();
+			}
+			if(x < *par->getKey())	par->setLeft(nd);
+			else	par->setRight(nd);
+			nd->setParent(par);
+			n++;
+			return this;
+		}
+
+		bool search(H x)
+		{
+			return _searchNode(x);
+		}
+
+		BSTree<H>* del(H x)
+		{
+			Node<H> *tmp = _searchNode(x);
 			if(tmp) _del(tmp);
 			return this;
 		}
 
-		bool search(H e)
-    {
-			return _search(root,e) != NULL;
+		H* begin()
+		{
+			current = _minimum(root);
+			if(current)	return current->getKey();
+			return NULL;
 		}
 
-    H* begin()
-    {
-      current = _minimum(root);
-      if(!current)  return NULL;
-      return current->getKey();
-    }
+		H* next()
+		{
+			if(current)	current = _succ(current);
+			if(!current) return NULL;
+			return current->getKey();
+		}
 
-    H* next()
-    {
-      if(current) current = _succ(current);
-      if(current) return current->getKey();
-      return NULL;
-    }
+		void print()
+		{
+			for(H *it = begin(); it; it = next())
+				cout << *it << " ";
+			cout << endl;
+		}
 
-    void print()
-    {
-        H *tmp = begin();
-        while(tmp)
-        {
-          cout << *tmp << " ";
-          tmp = next();
-        }
-        cout << endl;
-    }
+		void printPre()
+		{
+			_preOrder(root);
+			cout << endl;
+		}
+
+		void printPost()
+		{
+			_postOrder(root);
+			cout << endl;
+		}
 
 };
 
-//Overloading operator << BSTree
+/******************* Overloading operator << BSTree *******************/
 template<class H> ostream &operator<<(ostream &os, BSTree<H> &obj)
 {
-  for(H *it = obj.begin(); it; it = obj.next())
-    os << *it << " ";
-  return os << endl;
+	for(H *it = obj.begin(); it; it = obj.next())
+		os << *it << " ";
+	return os << endl;
 }
 
 /******************* Main *******************/
 int main()
 {
-    BSTree<int> *t = new BSTree<int>();
-    t->ins(4)->ins(1)->ins(6);
-    t->print();
-    cout << *t;
-    cout << t->search(4) << endl;
+	BSTree<int> *t = new BSTree<int>();
+	t->ins(1)->ins(3)->ins(7)->ins(2)->ins(8);
+	cout << *t;
+	t->print();
+	t->printPre();
+	t->printPost();
 
-    t->del(4);
-    t->print();
-    cout << *t;
-    cout << t->search(4) << endl;
+	cout << endl;
 
-    cin.get();
-    return 0;
+	t->del(1)->del(8);
+	cout << *t;
+	t->print();
+	t->printPre();
+	t->printPost();
+
+	cin.get();
+	return 0;
 }
