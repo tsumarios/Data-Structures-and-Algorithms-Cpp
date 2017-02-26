09357618ab8ba29d16964290aaf27b06b318161f
Mario Raciti <zumariodj@gmail.com>
2017-02-26 15:49:23 +0100

    Update ArrayStack.cpp

--- Code/Data-Structures/Lists/ArrayStack.cpp
+++ Code/Data-Structures/Lists/ArrayStack.cpp
@@ -8,67 +8,69 @@ ArrayStack Template
 
 using namespace std;
 
+
 /******************* Class ArrayStack *******************/
 template<class H> class ArrayStack
 {
-    H* s;
-    int length, n;
+		H *s;
+		int length, n;
 
-  public:
-    ArrayStack(int length = 100)
-    {
-      this->length = length;
-      s = new H[length];
-      n = 0;
-    }
-
-    ArrayStack<H>* push(H x)
-    {
-      if(isFull())  cout << "Lo stack è pieno!\n";
-      else{
-        s[n] = x;
-        n++;
-      }
-      return this;
-    }
-
-    H* pop()
-    {
-      if(isEmpty())  cout << "Lo stack è vuoto!\n";
-      else{
-        n--;
-        return &s[n];
-        }
-      return NULL;
-    }
-
-    bool isFull() {
-			return (length==n)?1:0;
+	public:
+		ArrayStack(int length = 100)
+		{
+			this->length = length;
+			s = new H[length];
+			n = 0;
 		}
 
-		bool isEmpty() {
-			return (n==0)?1:0;
+		ArrayStack<H>* push(H x)
+		{
+			if(isFull()) cout << "Stack is full!\n";
+			else
+			{
+				s[n] = x;
+				n++;
+			}
+			return this;
 		}
 
-		void print() {
+		H* pop()
+		{
+			if(isEmpty()) cout << "Stack is empty\n";
+			else
+			{
+				n--;
+				return &s[n];
+			}
+			return NULL;
+		}
+
+		bool isFull() { return length == n; }
+
+		bool isEmpty() { return n == 0; }
+
+		void print()
+		{
 			for(int i = 0; i < n; i++)
 				cout << s[i] << " ";
 			cout << endl;
 		}
 
 		int getSize() { return n; }
+
 };
+
 
 /******************* Main *******************/
 int main()
 {
-    ArrayStack<int> *a = new ArrayStack<int>();
-    a->push(3)->push(6)->push(2);
-    a->print();
-    a->pop();
-    a->print();
+	ArrayStack<int> *s = new ArrayStack<int>();
+    s->push(3)->push(6)->push(2);
+    s->print();
+    s->pop();
+    s->print();
 
 
-    cin.get();
-    return 0;
+	cin.get();
+	return 0;
 }
